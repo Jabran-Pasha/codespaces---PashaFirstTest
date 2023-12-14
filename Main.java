565d614a65d3ff62689d2f1b2efc0351960a6a70
Jabran-Pasha <146111817+Jabran-Pasha@users.noreply.github.com>
2023-12-14 17:03:50 +0000

    first commit

---
+++ Main.java
@@ -0,0 +1,10 @@
+public class Main{
+    public static void main(String[] args){
+        Stuff stuff = new Stuff(10);
+        stuff.printAddAmount(10);
+        System.out.println(stuff.getValue());
+
+
+
+    }
+}
